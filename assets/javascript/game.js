0390b3c519c98549fbe0f21ad3b17930cd67359c
Kathy <kepile@verizon.net>
2017-02-02 22:22:00 -0500

    code without pictures, music and just arrays - not object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,23 @@
 
-      // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+      // Sends current values to the web page including instructions, # wins, # losses, # guesses, lettr guessed .
       
-	function displayStatus(inst, win, loss, guess, lttr, word) {
+	function displayStatus(inst, win, loss, guess, lttr, word, cWord) {
     var formatWord = "";
     var formatLetter = "";
     
 
-
-    for (var q = 0; q < word.length; q++) {
-      formatWord += word[q] + " ";
+// Take the array of correctly guessed letters and store as a string
+    for (var q = 0; q < cWord.length; q++) {
+      console.log(cWord[q] + " cword - " + q + "   word - " + word[q]);
+      if (cWord[q] == " ") { formatWord += "&nbsp&nbsp&nbsp";}
+        else {
+          formatWord += word[q] + " ";}
+       console.log("++++++++ " + formatWord + " is in formatWord");
     }
 
+console.log("++++++++ " + formatWord + " is in formatWord");
 
+// Take the array of guessed letters, format it and store as a string
     for (var r = 0; r < lttr.length; r++) {
       if (r > 1) {
         console.log(r + " is adding a comma formatLetter = " + formatLetter);
@@ -31,7 +37,7 @@
 }
 
 
-// Check to see if input was a letter
+// Check to see if input was a capital or lowercase letter
   function checkUserInput (inp) {
     if ((inp > 64 && inp <91) || (inp > 96 && inp < 123)) {
       return true;
@@ -51,14 +57,15 @@
  
 // select a word that has not been used already and check if any words available ***not done yet
         var i = 1;
-         var selectWord;
+        var selectWord;
         console.log("enter while loop ");
         while (i > -1 ) {             //if index of 0 or greater returned, the word has been played
           console.log("while loop " + i);
            selectWord =  wordOptions[Math.floor(Math.random() * wordOptions.length)];     // Select a random word
            console.log("the word is " + selectWord);
-           i = wordsPlayed.indexOf(selectWord) }  // check if the word has been used.  If it has, loop again.
-   
+           i = wordsPlayed.indexOf(selectWord); }  // check if the word has been used.  If it has, loop again.
+           wordsPlayed.push(selectWord);
+
            console.log("word hasn't been used")
        
             currentWord = selectWord.split("");  // put the letters of the array into an array
@@ -82,8 +89,9 @@
                    if (currentWord[i] == " ") {
                     arrayhold[i] = currentWord[i];
 
-                   }
-                    else { arrayhold[i] = "_";}   
+                   } else { 
+
+                    arrayhold[i] = "_";}   
 
                     console.log(arrayhold[i])}  
                   correctLetters = arrayhold;
@@ -100,11 +108,12 @@
 	 
       // var counters {};
       var instruct;
-      var firstClick = 0
+      var firstClick = 0;
       var instruct;
       var winCount = 0;
       var lossCount = 0;
       var gameCount = 0;
+      var maxGames = 4;
       var wordOptions = ["Golden Girls", "The Cosby Show", "The Facts of Life", "Cheers"];
       // var musicstored = "http://www.sitcomsonline.com/sounds/thegoldenpalace.mp3";
       var wordsPlayed = [];
@@ -115,46 +124,57 @@
       var lowerCaseWord;
     
 
+ // Display initial instructs on the screen
+ 
    instruct = "Press any key to get started";
-	 displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters);
+	 displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters, currentWord);
 
- // This function is run whenever the user presses a key at the beginning of the game
+ 
+ // This function is run whenever the user presses a key 
    document.onkeyup = function(event) {
-      var userInp = String.fromCharCode(event.keyCode).toLowerCase();
+ 
        userInp = event.keyCode;
      
       console.log(userInp + " userInp before transform" );
    
- 
-// Check to see if this is the first time input was received and set up initial parameters
-   if (firstClick == 0 ) {
+  
+   
+// Check to see if this is the beginning of a new game and set up initial parameters
+   if (firstClick == 0) {
+          console.log("gameCount is " + gameCount);
+         if (gameCount >= maxGames) {
+             console.log("GAME IS OVER GAME IS OVER GAME IS OVER GAME IS OVER");
+               
+              
+              instruct = "You have played too many times.  Get a Life!";
+              displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters, currentWord);
+               var targetDiv = document.getElementById("instruction");
+               targetDiv.setAttribute("jumbotron", "background-color: red;");
+              return;
+           }
+              
           console.log("inside firstclick = 0 and games left to play");
           guessCount = 10;
           lettersGuess = [" "]; // array of all letters guessed so far
           firstClick++;
           gameCount ++;
-          console.log("user key hit to start game game count" + gameCount);
+
+          currentWord.length = 0  // houses an array of the word being guessed
+          correctLetters.lenght = 0; //displays on screen to open and guessed letters in a word console.log("user key hit to start game game count" + gameCount);
           selectWord(wordsPlayed);
 
           setWordDisplay(currentWord);
            console.log("letters to be displayed returned to program "+ correctLetters.toString());
           instruct = "Please take a guess";
-          displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters);}
+          // displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters, currentWord);
+        }
 
 
-     if  (checkUserInput(userInp)) {
+    else if  (checkUserInput(userInp)) {
 
          userInp = String.fromCharCode(event.keyCode).toLowerCase(); 
          console.log("*****" +userInp + " userInp after transform" );
-      // if (firstClick == 0 && gameCount < wordOptions.lenght - 1 ) {
      
-      // else if (firstClick == 0 && gameCount > wordOptions.lenght - 1 ) {
-      //    console.log("inside firstclick = 0 and NOOOO games left to play")
-      //     guessCount = 10;
-      //      gameCount = 0;
-      //      instruct = "You have guessed all the words!  Thank you for playing";
-      //      displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters) 
-          
           
           console.log("game being played.  User input " + userInp);
           console.log("lettersGuess index " + lettersGuess.indexOf(userInp));
@@ -190,25 +210,25 @@
 
               if (correctLetters.indexOf("_") == -1) {
               instruct = "GAME IS OVER!  YOU WON";
-                    currentWord.length = 0  // houses an array of the word being guessed
-                    correctLetters.lenght = 0; //displays on screen to open and guessed letters in a word
                     winCount++;
                     firstClick = 0;
-
+                    
          
 
               } else if (guessCount <= 0) {
-                    instruct = "GAME IS OVER!  YOU LOST.  THE WORD WAS";
-                          currentWord.length = 0  // houses an array of the word being guessed
-                    correctLetters.lenght = 0; //displays on screen to open and guessed letters in a word
+                    instruct = "GAME IS OVER!  YOU LOST.  The word was " + currentWord + ".  Press any key to continue.";
+                    
                     lossCount++;
                     firstClick = 0;
+                    
               }  
            
-                 displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters);
+                 displayStatus(instruct, winCount, lossCount, guessCount, lettersGuess, correctLetters, currentWord);
           
        }
     
+
+
 
            
 // var str = "Hello world, welcome to the universe.";
